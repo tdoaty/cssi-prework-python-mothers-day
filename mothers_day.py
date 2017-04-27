9603a8ec29acf2d6a1bd8b024a3aebe8ffdf0724
tdoaty <tdoaty@google.com>
2017-04-27 14:07:15 -0400

    working on mothers_day.py

---
+++ mothers_day.py
@@ -0,0 +1,8 @@
+def mothers_day():
+    return "Happy Mother's Day, Mom!"
+def better_mothers_day(name):
+    return "Happy Mother's Day, " + name + "!"
+def best_mothers_day(name = "Mom"):
+    return "Happy Mother's Day, " + name + "!"
+def holiday_greeting(to, from, holiday):
+    return "Happy " {holiday} + ", " + to + "!" + " - From" + from + "."
